18c0686f1c46a458ad478442b4f8aab2330c04cf
yngvrr <igor4711@ya.ru>
2019-07-06 19:20:10 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,4 +4,28 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+// foreach($a as $k => $b){ $s[$b]= $a[$b-1];  }
+//>>> foreach($s as $k => $v){ $p = $s[$v]; $m = $s[$p]; $h[]=[$k =>$v, $v=>$p, $p=>$m]; }
+var love = [];
+var lovetriangle = 0;
+preferences.forEach(function (spichoneesloves){
+  //console.log(preferences);
+    love[spichoneesloves] = preferences[spichoneesloves-1];
+  //  console.log(love);
+   });
+
+love.forEach(function (spichoneslove, spichone){
+  secondsspichoneslove = love[spichoneslove]
+  thirdspichoneslove = love[secondsspichoneslove]
+if(thirdspichoneslove==spichone && spichoneslove !=spichone){
+lovetriangle++;
+//console.log(lovetriangle);
+}});
+
+  
+
+
+  var ammount = lovetriangle/3;
+
+  return ammount;
 };
